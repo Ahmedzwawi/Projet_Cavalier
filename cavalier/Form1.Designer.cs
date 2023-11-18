63fb73d385ed4842f1087ae5196820d3cc60c647
ZWAWI Ahmed <ahmedzwawi46@gmail.com>
2023-11-18 15:02:26 +0100

    troisieme commit

---
+++ cavalier/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Cavalier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Commencer = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.règlesDuJeuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aProposToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Commencer
+            // 
+            this.Commencer.Location = new System.Drawing.Point(675, 395);
+            this.Commencer.Name = "Commencer";
+            this.Commencer.Size = new System.Drawing.Size(113, 30);
+            this.Commencer.TabIndex = 0;
+            this.Commencer.Text = "JOUEZ";
+            this.Commencer.UseVisualStyleBackColor = true;
+            this.Commencer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(416, 207);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome to Euler\'s Knight";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.règlesDuJeuToolStripMenuItem,
+            this.aProposToolStripMenuItem,
+            this.quitterToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // règlesDuJeuToolStripMenuItem
+            // 
+            this.règlesDuJeuToolStripMenuItem.Name = "règlesDuJeuToolStripMenuItem";
+            this.règlesDuJeuToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.règlesDuJeuToolStripMenuItem.Text = "Rules";
+            this.règlesDuJeuToolStripMenuItem.Click += new System.EventHandler(this.règlesDuJeuToolStripMenuItem_Click);
+            // 
+            // aProposToolStripMenuItem
+            // 
+            this.aProposToolStripMenuItem.Name = "aProposToolStripMenuItem";
+            this.aProposToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aProposToolStripMenuItem.Text = "About";
+            this.aProposToolStripMenuItem.Click += new System.EventHandler(this.aProposToolStripMenuItem_Click);
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.quitterToolStripMenuItem.Text = "Exit";
+            this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Commencer);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Commencer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem règlesDuJeuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aProposToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+    }
+}
+
---
+++ cavalier/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Cavalier
+{
+    public partial class Form1 : Form
+    {
+        static int[] depi = new int[] { 2, 1, -1, -2, -2, -1, 1, 2 };
+        static int[] depj = new int[] { 1, 2, 2, 1, -1, -2, -2, -1 };
+        //random de 0 à 2
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Button[,] grille;
+      
+       
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.BackColor = Color.Gold;
+            this.Text = "Menu | Euler's Knight";
+            button1.Text = "";
+         button1.BackgroundImage = Image.FromFile("img/cava.jpg");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void règlesDuJeuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string regles = "FR: Le but de ce jeu est de faire parcourir un cavalier sur l'ensemble des cases d'un échiquier sans passer deux fois sur la même case.\n\n EN: The Euler's Knight game involves moving a knight on a chessboard to visit every square exactly once, following the rules of chess Knight moves. ";
+            MessageBox.Show(regles);
+        }
+
+        private void aProposToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string about = "Jeu du cavalier d'Euler développé par Ahmed zwawi";
+            MessageBox.Show(about);
+        }
+
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
